e6bea9713207a027706284e6e666dd1d3cb6ceba
code2304104 <jkazembe2304@gmail.com>
2024-05-27 18:18:32 +0300

    listpage
    ni muhimuuuu

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,38 +2,36 @@ import { useState } from "react";
 import "./navbar.scss";
 
 function Navbar() {
-    const [open,setOpen] = useState(false)
-    return(
+    const [open, setOpen] = useState(false);
+    return (
         <nav>
             <div className="left">
                 <a href="/" className="logo">
-                    <img src="/logo.png" alt=""/>
+                    <img src="/logo.png" alt="Logo" />
                     <span>ePMS</span>
                 </a>
                 <a href="/">Home</a>
                 <a href="/">About</a>
                 <a href="/">Contacts</a>
-                <a href="/">Agents</a>  
+                <a href="/">Agents</a>
             </div>
             <div className="right">
-            <a href="/">sign in</a>
-            <a href="/" className="register">sign up</a>
-            <div className="menuIcon">
-                <img src="/menu.png" alt="" onClick={() => setOpen((prev)=>!prev)}/>
-            </div>
-            <div className={open ? "menu active" : "menu"}>
-                <a href="/">Home</a>
-                <a href="/">About</a>
-                <a href="/">Contacts</a>
-                <a href="/">Agents</a>
-                <a href="/">sign in</a>
-                <a href="/">sign up</a>
-
-            </div>
-
+                <a href="/">Sign In</a>
+                <a href="/" className="register">Sign Up</a>
+                <div className="menuIcon">
+                    <img src="/menu.png" alt="Menu" onClick={() => setOpen((prev) => !prev)} />
+                </div>
+                <div className={open ? "menu active" : "menu"}>
+                    <a href="/">Home</a>
+                    <a href="/">About</a>
+                    <a href="/">Contacts</a>
+                    <a href="/">Agents</a>
+                    <a href="/">Sign In</a>
+                    <a href="/">Sign Up</a>
+                </div>
             </div>
         </nav>
-    )
+    );
 }
 
 export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import "./index.scss"
-import "./layout.scss"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import "./index.scss";
+import "./routes/layout/layout.scss";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
